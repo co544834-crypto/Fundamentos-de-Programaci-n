96c6fc13f81645e87819d8e8ea62e53048ddb089
co544834-crypto <co544834@uaeh.edu.mx>
2025-09-03 00:07:00 -0600

    Add files via upload

---
+++ postres.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class postres {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String tipoChocolate;
+        int precioBase = 0;
+        int precioTotal;
+
+        System.out.print("¿De qué sabor quiere la tarta? (manzana, fresa, chocolate): ");
+        String s = sc.next().toLowerCase();
+
+        switch (s) {
+            case "manzana":
+                precioBase = 200;
+                break;
+            case "fresa":
+                precioBase = 250;
+                break;
+            case "chocolate":
+                System.out.print("¿Qué tipo de chocolate? (negro/blanco): ");
+                tipoChocolate = sc.next().toLowerCase();
+                if (tipoChocolate.equals("negro")) {
+                    precioBase = 280;
+                } else {
+                    precioBase = 300;
+                }
+                break;
+            default:
+                System.out.println("Sabor no válido.");
+                sc.close();
+                return;
+        }
+
+        System.out.print("¿Cuántos snacks desea añadir? (cada uno 25): ");
+        int snacks = sc.nextInt();
+
+        System.out.print("¿Desea personalizar con un nombre? (true/false): ");
+        boolean nombre = sc.nextBoolean();
+
+        precioTotal = precioBase + (snacks * 25);
+        if (nombre) {
+            precioTotal += 30;
+        }
+
+        System.out.println("El precio total de la tarta es: " + precioTotal);
+
+        sc.close();
+    }
+    
+}
